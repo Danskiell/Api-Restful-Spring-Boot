c0aaabd86cb0e44d23dee3811798c7f3f26facbb
Daniel Alves de Moura <142283468+Danskiell@users.noreply.github.com>
2024-11-16 14:05:48 -0300

    Remoções

--- src/main/java/com/restful/snackapi/repository/MovComaGeraRepository.java
+++ src/main/java/com/restful/snackapi/repository/MovComaGeraRepository.java
@@ -1,5 +1,4 @@
 package com.restful.snackapi.repository;
-import com.restful.snackapi.model.Mov_Coma_Gera;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
